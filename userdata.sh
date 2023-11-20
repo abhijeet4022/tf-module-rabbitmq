8665b393f4782ced951a65140b7a619a353490f6
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-20 08:11:40 +0530

    Terraform: Roboshop-App Terraform

--- userdata.sh
+++ userdata.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 yum install ansible -y  &>> /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/abhijeet4022/roboshop-ansible -e "component=rabbitmq" main.yml &>> /opt/userdata.log
+
